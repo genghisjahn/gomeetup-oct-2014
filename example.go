c25409bb09252117dabc2118ec4c6a6edb13b54c
Jon Wear <jonwear@fastmail.fm>
2014-10-07 21:36:17 -0400

    slide updates

--- example.go
+++ example.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"flag"
+
+	"code.google.com/p/gcfg"
 )
 
-type Thing struct {
-	Id   int
-	Name string
-}
-type ThingErr struct {
-	Thing
-}
-
 func main() {
-	thing := Thing{5, "Item"}
-	if num, err := strconv.Atoi(thing); err != nil {
-		PrintError(err)
-	} else {
-		fmt.Println("Num:", num)
-	}
+
 }
 
-func PrintError(err error) {
-	fmt.Printf("Error: %v\n", err)
+func NamedFlags1() {
+	namePtr := flag.String("name", "George", "The name of a person")
+	agePtr := flag.Int("age", 40, "The age of a person")
+	honestPtr := flag.Bool("honest", false, "Bool for honesty")
+	flag.Parse() //If you don't call this, flags aren't assigned.
+}
+
+func NamedFlags2() {
+	var name string
+	flag.StringVar(&name, "name", "George", "The name of a person")
+	flag.Parse() //If you don't call this, flags aren't assigned.
+}
+
+func PosArgs() {
+	posArgs := flag.Args()
+	flag.Parse()
+}
+
+func Config1() {
+	type Config struct {
+		Section struct {
+			Name string
+			Flag bool
+		}
+	}
+
+	var cfg Config
+	err := gcfg.ReadFileInto(&cfg, "myconfig.gcfg")
 }
